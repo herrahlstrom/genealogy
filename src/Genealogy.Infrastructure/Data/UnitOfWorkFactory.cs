ae8e690170172706b20ea0e2e96049f396d58a9e
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-11-03 15:04:02 +0100

    Add unit of work

---
+++ src/Genealogy.Infrastructure/Data/UnitOfWorkFactory.cs
@@ -0,0 +1,29 @@
+﻿using Genealogy.Domain.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Genealogy.Infrastructure.Data;
+
+internal class UnitOfWorkFactory : IUnitOfWorkFactory
+{
+    private readonly IDbContextFactory<GenealogyDbContext> _factory;
+    private bool migrated = false;
+
+    public UnitOfWorkFactory(IDbContextFactory<GenealogyDbContext> dbContextFactory)
+    {
+        _factory = dbContextFactory;
+    }
+
+    IUnitOfWork IUnitOfWorkFactory.CreateDbContext()
+    {
+        lock(_factory)
+        {
+            var dbContext = _factory.CreateDbContext();
+            if (!migrated)
+            {
+                dbContext.Database.Migrate();
+                migrated = true;
+            }
+            return new UnitOfWork(dbContext);
+        }
+    }
+}
